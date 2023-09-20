e234d9516c8952a6e3aff00ec7b8652b9772a853
Lucas Gomes <lucas.gomes@intellisense.io>
2023-09-19 22:24:15 -0300

    feat: separa a estruta de emcapsulamento do jogo

--- jogo-da-velha/src/Main.hs
+++ jogo-da-velha/src/Main.hs
@@ -2,20 +2,17 @@ module Main where
 
 import Graphics.Gloss
 
+-- janela do jogo
+gameWindow = InWindow "Tic-tac-toe Functional - Grupo 01" (700, 525) (100, 100)
+
 -- cor da janela do jogo
 backgroundColor = makeColor 164 187 222 255
-
---estado inicial do jogo
-initialGame = 0
 
 --o estado do jogo em uma imagem a ser exibida na tela
 gameAsPicture _ = Blank
 
 -- transforma o estado do jogo (não faz nada por enquanto)
 transformGame _ game = game
-
--- janela do jogo
-gameWindow = InWindow "Tic-tac-toe Functional - Grupo 01" (700, 525) (100, 100)
 
 main :: IO()
 main = play gameWindow backgroundColor 30 initialGame gameAsPicture transformGame (const id)
