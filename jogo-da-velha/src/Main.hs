8f315020c4b21d5f0d3f1d227c4702887614365f
sidney092k@gmail.com <sidney092k@gmail.com>
2023-09-21 22:39:50 -0300

    fix: corrige bugs

--- jogo-da-velha/src/Main.hs
+++ jogo-da-velha/src/Main.hs
@@ -1,21 +1,24 @@
 module Main where
 
 import Graphics.Gloss
-
+import Graphics.Gloss.Data.Color
 import Game
-import Rendering as Show
+import Rendering
+import Logic
 
 -- janela do jogo
-gameWindow = InWindow "Tic-tac-toe Functional - Grupo 01" (700, 525) (100, 100)
+gameWindow = InWindow "Tic-tac-toe Functional - Grupo 01" (screenWidth, screenHeight) (100, 100)
 
 -- cor da janela do jogo
-backgroundColor = makeColor 164 187 222 255
+backgroundColor = makeColor 0 0 0 255
 
---o estado do jogo em uma imagem a ser exibida na tela como vazio
-gameAsPicture _ = Blank
+-- --o estado do jogo em uma imagem a ser exibida na tela como vazio
+-- gameAsPicture _ = Blank
 
--- transforma o estado do jogo (não faz nada por enquanto)
-transformGame _ game = game
+-- -- transforma o estado do jogo (não faz nada por enquanto)
+-- transformGame _ game = game
+
+
 
 main :: IO()
-main = play gameWindow backgroundColor 30 initialGame Show.gameAsPicture transformGame (const id)
+main = play gameWindow backgroundColor 30 initialGame gameAsPicture transformGame (const id)
